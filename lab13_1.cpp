564c38a939141c6216c4dcbc1bce830b3fde72f7
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-01-02 15:24:58 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,32 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double data[], int n, double result[]) {
+  double sum = 0;
+  double squaredSum = 0;
+  double mul = 1;
+  double recipSum = 0;
+  double minn = data[0];
+  double maxx = data[0];
+  for (int i = 0; i < n; i++) {
+    double d = data[i];
+    sum += d;
+    squaredSum += d * d;
+    mul *= d;
+    recipSum += 1 / d;
+
+    if (d < minn) minn = d;
+    if (d > maxx) maxx = d;
+  }
+
+  double mean = sum / n;
+  double squaredMean = squaredSum / n;
+
+  result[0] = mean;
+  result[1] = sqrt(squaredMean - mean*mean);
+  result[2] = pow(mul, 1.0/n);
+  result[3] = n / recipSum;
+  result[4] = maxx;
+  result[5] = minn;
+}
